62e53b11838bd983a36f723f41109866d85fbe2b
Yuji Toshinaga <y.toshinaga@gmail.com>
2017-04-22 22:54:28 +0900

    GPFSELのマスク処理考慮漏れを修正

--- JtagCon/main.c
+++ JtagCon/main.c
@@ -9,7 +9,9 @@ int main(void)
   volatile unsigned int tmp;
 
 	tmp = *((const volatile unsigned int *)GPFSEL1);
-  *(volatile unsigned int *)GPFSEL1 = tmp | (0x01 << (3 * 6));
+  tmp &= ~(0x7 << (3 * 6));
+  tmp |= (0x01 << (3 * 6));
+  *(volatile unsigned int *)GPFSEL1 = tmp;
 
   *(volatile unsigned int *)GPSET0 = 0x01 << 16;
 
